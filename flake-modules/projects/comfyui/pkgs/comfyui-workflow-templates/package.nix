c937b005ecc220b767d4408b585247e13a421b06
Spiros Mantzavinos <smantzavinos@gmail.com>
2025-12-05 23:55:58 -0500

    comfyui: update to v0.3.77 and fix workflow-templates dependencies
    Updates ComfyUI from v0.3.65 to v0.3.77 and resolves breaking changes in
    the comfyui-workflow-templates package structure.
    
    Changes:
    - Updated ComfyUI core to v0.3.77
    - Updated comfyui-embedded-docs to 0.3.1
    - Updated comfyui-frontend-package to 1.34.6
    - Updated comfyui-workflow-templates to 0.7.49
    
    The workflow-templates package introduced breaking changes starting from
    version 0.3.0, splitting functionality into multiple packages. Added
    inline builds for all required dependencies:
    - comfyui-workflow-templates-core (0.3.20)
    - comfyui-workflow-templates-media-api (0.3.18)
    - comfyui-workflow-templates-media-video (0.3.13)
    - comfyui-workflow-templates-media-image (0.3.24)
    - comfyui-workflow-templates-media-other (0.3.34)
    
    These packages are built inline to avoid circular dependency issues
    between comfyuiPackages and python3Packages scopes.

--- flake-modules/projects/comfyui/pkgs/comfyui-workflow-templates/package.nix
+++ flake-modules/projects/comfyui/pkgs/comfyui-workflow-templates/package.nix
@@ -4,6 +4,72 @@
 }:
 let
   npin = comfyuiNpins.comfyui-workflow-templates;
+  
+  # Helper to build workflow template packages
+  buildTemplatePackage = {
+    pname,
+    version,
+    hash,
+    dependencies ? [],
+  }: python3Packages.buildPythonPackage rec {
+    inherit pname version;
+    
+    src = python3Packages.fetchPypi {
+      inherit pname version hash;
+    };
+
+    format = "pyproject";
+
+    build-system = with python3Packages; [
+      setuptools
+    ];
+
+    propagatedBuildInputs = dependencies;
+
+    pythonImportsCheck = [
+      (builtins.replaceStrings ["-"] ["_"] pname)
+    ];
+
+    meta = with python3Packages.lib; {
+      description = "ComfyUI workflow templates package";
+      homepage = "https://github.com/Comfy-Org/workflow_templates";
+      license = licenses.mit;
+    };
+  };
+
+  core = buildTemplatePackage {
+    pname = "comfyui_workflow_templates_core";
+    version = "0.3.20";
+    hash = "sha256-YtEqM0w8SzoTN92NJBXw8YS48dzqhiikuwEmd8RxF/k=";
+  };
+
+  media-api = buildTemplatePackage {
+    pname = "comfyui_workflow_templates_media_api";
+    version = "0.3.18";
+    hash = "sha256-Xc01ClqtjnEBFMiJgreSicLaEOECJZzPh59VWTRWSaI=";
+    dependencies = [ core ];
+  };
+
+  media-video = buildTemplatePackage {
+    pname = "comfyui_workflow_templates_media_video";
+    version = "0.3.13";
+    hash = "sha256-hQIS00FSvYY+z1oURTfd+/Q6dg6h9+2szIRbl1SobCc=";
+    dependencies = [ core media-api ];
+  };
+
+  media-image = buildTemplatePackage {
+    pname = "comfyui_workflow_templates_media_image";
+    version = "0.3.24";
+    hash = "sha256-V7sqWCdfqYmQ1Od3hGtnMmEqTZSOt0Kz1BsMhNqE8Vk=";
+    dependencies = [ core media-api ];
+  };
+
+  media-other = buildTemplatePackage {
+    pname = "comfyui_workflow_templates_media_other";
+    version = "0.3.34";
+    hash = "sha256-HMCTfe1JY4yXr/WjkxwifKz2CcQ3UNSVbS6bdXFXQpY=";
+    dependencies = [ core media-api ];
+  };
 in
 python3Packages.callPackage (
   {
@@ -20,7 +86,19 @@ python3Packages.callPackage (
       sha256 = npin.hash;
     };
 
-    format = "setuptools";
+    format = "pyproject";
+
+    build-system = with python3Packages; [
+      setuptools
+    ];
+
+    dependencies = [
+      core
+      media-api
+      media-video
+      media-image
+      media-other
+    ];
 
     pythonImportsCheck = [
       pname
